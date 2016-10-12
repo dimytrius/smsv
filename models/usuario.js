23a4d4db5dd6e861d6624f25f05571f72167f9ca
Gláuber Brennon <glauberbrennon@gmail.com>
2016-10-12 18:56:32 -0300

    Projeto quase pront

--- models/usuario.js
+++ models/usuario.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
 	var usuario = Schema({
 		nome: {type: String, required:true},
 		email: {type: String, required:true},
-		//senha: {type: String, required:true},
+		senha: {type: String, required:true},
 		pacientes: [paciente]
 	});
 
